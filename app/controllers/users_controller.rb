9b1fd96b8fc3c0c3c179e5ae5cf089489e4dd393
h-tsumuraya <hironori-tsumuraya@united.jp>
2016-03-24 12:13:48 +0900

    一旦コミット

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
-  
+
   def create
     @user = User.new(user_params)
     if @user.save
@@ -17,12 +17,39 @@ class UsersController < ApplicationController
       render "new"
     end
   end
-  
+
+  def edit
+    @user = User.find(params[:id])
+    # logger.debug("sessionのuser_id #{session[:user_id]}")
+    # logger.debug("paramsのid #{params[:id]}")
+    # if session[:user_id] == params[:id]
+    #   logger.debug("OK")
+    #   @user = User.find(params[:id])
+    # else
+    #   logger.debug("NG")
+    #   redirect_to root_path
+    # end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params_update)
+      flash[:success] = "成功"
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
   private
-  
+
   def user_params
     params.require(:user).permit(:name, :email,
                                  :password, :password_confirmation)
   end
-  
+
+  def user_params_update
+    params.require(:user).permit(:name, :email, :profile)
+  end
+
 end
